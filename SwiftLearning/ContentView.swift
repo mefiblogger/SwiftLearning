84d06c391fde629c86d53b980b3698cd85d23d79
Gabor Nadai <gabor.nadai@gmail.com>
2020-03-23 15:30:58 +0100

    Creating project

---
+++ SwiftLearning/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftLearning
+//
+//  Created by Gabor Nadai on 2020. 03. 23..
+//  Copyright © 2020. Mefi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
